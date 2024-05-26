22fef2ffe52d1ee6c2ec0bb9c0548b99a6548d2b
Aba <sarah.aba@hotmail.fr>
2024-05-26 11:19:19 +0200

    modification error logement ID null

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,13 +4,11 @@ import Header from "./components/Header";
 
 function Layout(props) {
   return (
-    <>
-      <div>
-        <Header />
-        <div>{props.children}</div>
-        <Footer />
-      </div>
-    </>
+    <div>
+      <Header />
+      <div>{props.children}</div>
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import Description from "../components/Description";
 import LogementRating from "../components/LogementRating";
 import Slideshow from "../components/Slideshow";
 import Tag from "../components/Tag";
 import { getData } from "../utils/api";
+import Error404 from "./Error404";
 import styles from "./Logement.module.scss";
 
 function Logement() {
@@ -21,67 +21,71 @@ function Logement() {
 
   return (
     <>
-      <div className={styles.cadreCarouselInfos}>
-        <div>{logement && <Slideshow images={logement.pictures} />}</div>
-        <div className={styles.gap}>
-          <div className={styles.cadreInfos}>
+      {!logement ? (
+        <Error404 />
+      ) : (
+        <div className={styles.cadreCarouselInfos}>
+          <div>{logement && <Slideshow images={logement.pictures} />}</div>
+          <div className={styles.gap}>
+            <div className={styles.cadreInfos}>
+              {data && (
+                <>
+                  <div className={styles.cadreInfosUn}>
+                    <h2>{logement.title}</h2>
+                    <p>{logement.location}</p>
+                  </div>
+                </>
+              )}
+              <div className={styles.cadreTag}>
+                {data &&
+                  logement.tags.map((el, index) => (
+                    <Tag key={index} message={el} />
+                  ))}
+              </div>
+            </div>
+            <div className={styles.cadreTagStras}>
+              <div className={styles.cadreInfosDeux}>
+                <div>
+                  {logement && logement.host && (
+                    <>
+                      <h3>{firstName}</h3>
+                      <h3>{lastName}</h3>
+                    </>
+                  )}
+                </div>
+                <img
+                  className={styles.imgHost}
+                  src={logement && logement.host && logement.host.picture}
+                  alt="host-picture"
+                />
+              </div>
+              {logement && <LogementRating rating={logement.rating} />}
+            </div>
+          </div>
+          <div className={styles.cadre}>
             {data && (
               <>
-                <div className={styles.cadreInfosUn}>
-                  <h2>{logement.title}</h2>
-                  <p>{logement.location}</p>
-                </div>
+                <Description
+                  className={styles.description}
+                  title="Description"
+                  textDescription={logement.description}
+                />
+                <Description
+                  className={styles.equipements}
+                  title="Equipement"
+                  textDescription={
+                    <ul className={styles.ul}>
+                      {logement.equipments.map((el, index) => (
+                        <li key={index}>{el}</li>
+                      ))}
+                    </ul>
+                  }
+                />
               </>
             )}
-            <div className={styles.cadreTag}>
-              {data &&
-                logement.tags.map((el, index) => (
-                  <Tag key={index} message={el} />
-                ))}
-            </div>
-          </div>
-          <div className={styles.cadreTagStras}>
-            <div className={styles.cadreInfosDeux}>
-              <div>
-                {logement && logement.host && (
-                  <>
-                    <h3>{firstName}</h3>
-                    <h3>{lastName}</h3>
-                  </>
-                )}
-              </div>
-              <img
-                className={styles.imgHost}
-                src={logement && logement.host && logement.host.picture}
-                alt="host-picture"
-              />
-            </div>
-            {logement && <LogementRating rating={logement.rating} />}
           </div>
         </div>
-        <div className={styles.cadre}>
-          {data && (
-            <>
-              <Description
-                className={styles.description}
-                title="Description"
-                textDescription={logement.description}
-              />
-              <Description
-                className={styles.equipements}
-                title="Equipement"
-                textDescription={
-                  <ul className={styles.ul}>
-                    {logement.equipments.map((el, index) => (
-                      <li key={index}>{el}</li>
-                    ))}
-                  </ul>
-                }
-              />
-            </>
-          )}
-        </div>
-      </div>
+      )}
     </>
   );
 }
